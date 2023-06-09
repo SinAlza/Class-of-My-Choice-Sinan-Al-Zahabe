98a8f0f5fd820f747ac54c84a1b339ff865e2ced
Sinan Al-zahabe <elemental-dragoon@hotmail.com>
2023-06-09 10:19:02 -0400

    Add project files.

---
+++ Necromancer.cs
@@ -0,0 +1,210 @@
+﻿namespace Class_of_My_Choice_Sinan_Al_Zahabe
+{
+    public class Necromancer
+    {
+
+        //Properties
+
+        // The character's name
+        public string Name { get; set; }
+
+        // The character's health
+        public int Health { get; set; }
+
+        // The character's mana
+        public int Mana { get; set; }
+
+        // The character's strength
+        public int Strength { get; set; }
+
+        // The character's dexterity
+        public int Dexterity { get; set; }
+
+        // The character's intelligence
+        public int Intelligence { get; set; }
+
+        // The character's wisdom
+        public int Wisdom { get; set; }
+
+        // The character's constitution
+        public int Constitution { get; set; }
+
+        // the chracter's defense
+        public int Defense { get; set; }
+
+
+
+        //Constructor
+
+        // Create a new Necromancer object
+        public Necromancer(string name, int health, int mana, int strength, int dexterity, int intelligence, int wisdom, int constitution, int defense)
+        {
+            Name = name;
+            Health = health;
+            Mana = mana;
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+            Wisdom = wisdom;
+            Constitution = constitution;
+            Defense = defense;
+        }
+
+
+
+        // Methods
+
+        // Attack an enemy
+        public void Attack()
+        {
+            Health -= Program.ally.Strength - Defense;
+            Console.WriteLine("You Slashed the enemy with your scyth.");
+            Console.ReadLine();
+
+            //// TODO: Implement attack method
+            //// This method will calculate the damage that the character deals to an enemy.
+            //// Get the enemy's health
+            //int enemyHealth = Health;
+
+            //// Calculate the damage that the Necromancer deals to the enemy
+            //int damage = Strength - Defense;
+
+            //// Reduce the enemy's health by the damage dealt
+            //enemyHealth -= damage;
+
+            // Check if the enemy is dead
+            if (Health <= 0)
+            {
+                // The enemy is dead
+                Console.WriteLine("You have defeated the enemy!");
+            }
+
+        }
+        public void EnemyAttack()
+        {
+            Health -= Program.enemy.Strength - Defense;
+
+
+            if (Health <= 0)
+            {
+                // The enemy is dead
+                Console.WriteLine("You have Died...");
+            }
+
+        }
+
+        // Cast Powers
+        public void UsePower()
+        {
+            Health -= Program.ally.Intelligence - Defense;
+            Program.ally.Mana -= 50;
+            Console.WriteLine("You cast Toll of the Dead.");
+            Console.ReadLine();
+            //// TODO: Implement Use Power method
+            //// This method will calculate the damage that the character deals to an enemy, or the effect that the character has on the environment.
+            //// Get the Power's damage
+            //int powerDamage = Intelligence;
+
+            //// Get the enemy's health
+            //int enemyHealth = Health;
+
+            //// Reduce the enemy's health by the Power damage
+            //enemyHealth -= powerDamage;
+
+            // Check if the enemy is dead
+            if (Health <= 0)
+            {
+                // The enemy is dead
+                Console.WriteLine("Your Power has defeated the enemy!");
+            }
+        }
+
+        public void EnemyPower()
+        {
+            Health -= Program.enemy.Intelligence - Defense;
+            Program.enemy.Mana -= 60;
+            Console.WriteLine("You cast Eldritch Blast.");
+
+            if (Health <= 0)
+            {
+                // The enemy is dead
+                Console.WriteLine("You have Died....");
+            }
+        }
+
+        // Take damage
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+
+        // Restore health
+        public void RestoreHealth()
+        {
+            Health += 50;
+            Console.WriteLine("You drank a Potion.");
+        }
+
+        // Restore mana
+        public void RestoreMana()
+        {
+            Mana += 50;
+            Console.WriteLine("You drank an Ether.");
+        }
+
+        //Enemy Attack Method
+        public void AttackPlayer()
+        {
+            // Get the player's health
+            int playerHealth = Health;
+
+            // Calculate the damage that the enemy deals to the player
+            int damage = Strength - Defense;
+
+            // Reduce the player's health by the damage dealt
+            playerHealth -= damage;
+
+            // Check if the player is dead
+            if (playerHealth <= 0)
+            {
+                // The player is dead
+                Console.WriteLine("You have been defeated!");
+            }
+
+
+            // Get the player's health
+            int NecromancerHealth = Health;
+
+            // Calculate the damage that the enemy deals to the player
+            int damage1 = Strength - Defense;
+
+            // Get a random number between 0 and 1
+            Random random = new Random();
+            int randomNumber = random.Next(0, 5);
+
+            // Check if the enemy's accuracy is greater than the random number
+            if (5 > randomNumber)
+            {
+                // The enemy hits the player
+                playerHealth -= damage1;
+
+                // Check if the player is dead
+                if (playerHealth <= 0)
+                {
+                    // The player is dead
+                    Console.WriteLine("You have been defeated!");
+                }
+            }
+            else
+            {
+                // The enemy misses the player
+                Console.WriteLine("You Dodged the Enemy's attack!");
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Class_of_My_Choice_Sinan_Al_Zahabe
+{
+    internal class Program
+    {
+
+        public static Necromancer enemy;
+        public static Necromancer ally;
+        public static void Main(string[] args)
+        {
+
+            // Create a new Necromancer object
+            Necromancer character = new Necromancer("undead Sage", 150, 300, 25, 20, 50, 50, 25, 10);
+            ally = character;
+            // Create a list of enemies
+            List<Necromancer> enemies = new List<Necromancer>();
+            enemies.Add(new Necromancer("Goblin", 50, 50, 15, 5, 5, 0, 0, 15));
+            enemies.Add(new Necromancer("Orc", 100, 100, 25, 10, 10, 0, 0, 20));
+            enemies.Add(new Necromancer("Dragon", 200, 200, 40, 20, 20, 20, 20, 20));
+
+            // Create a random number generator
+            Random random = new Random();
+
+            // Generate a random enemy
+            int enemyIndex = random.Next(enemies.Count);
+            enemy = enemies[enemyIndex];
+
+            // Print the enemy's name and health
+            Console.WriteLine("The enemy is a {0} with {1} health!", enemy.Name, enemy.Health);
+
+            // Create a loop that allows the user to repeatedly attack, Use Power, take damage, restore health, and restore mana
+            while (character.Health > 0 && enemy.Health > 0)
+            {
+                Console.WriteLine(ally.Name + " Health: " + ally.Health + " Mana:" + ally.Mana);
+                Console.WriteLine(enemy.Name + " Health: " + enemy.Health + " Mana:" + enemy.Mana);
+                // Get the user's input
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1. Attack");
+                Console.WriteLine("2. Use Power");
+                Console.WriteLine("3. Take damage");
+                Console.WriteLine("4. Restore health");
+                Console.WriteLine("5. Restore mana");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                // Perform the user's action
+                switch (input)
+                {
+                    case 1:
+                        enemy.Attack();
+                        break;
+                    case 2:
+                        enemy.UsePower();
+                        break;
+                    case 3:
+
+                        Random randoms = new Random();
+                        int newRandom = randoms.Next(1, 11);
+                        if (newRandom < 6)
+                        {
+                            character.EnemyAttack();
+                        }
+                        else
+                        {
+                            character.EnemyPower();
+                        }
+                        break;
+                    case 4:
+                        character.RestoreHealth();
+                        break;
+                    case 5:
+                        character.RestoreMana();
+                        break;
+                }
+                Console.Clear();
+            }
+
+            // Check if the character or enemy is still alive
+            if (character.Health > 0)
+            {
+                Console.WriteLine("The character has defeated the enemy!");
+            }
+            else
+            {
+                Console.WriteLine("The enemy has defeated your character!");
+            }
+        }
+    }
+}
